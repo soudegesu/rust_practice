0ac88ea1b56697504d48bf739011a70c46d25931
soudegesu <soudegesu@hotmail.co.jp>
2018-10-31 09:56:47 +0900

    add code sample guess number from input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello World");
+    println!("Guess the number!");
+
+    println!("Please input input your guess");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
